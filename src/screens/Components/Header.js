0fe4354db368061e0a895f60ef11427074b6910e
Thqmaz <t.groenendijk.tg@gmail.com>
2022-01-11 00:28:31 +0100

    created the header component

---
+++ src/screens/Components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Container, Nav, Navbar } from 'react-bootstrap';
+// import { auth } from '../../firebase';
+
+// import { Logout } from '../../functions/Logout';
+
+// function logOut() {
+//     auth.signOut();
+// }
+
+export const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Pterodactyl Install Manager</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/install">Installatie</Nav.Link>
+                        <Nav.Link href="/history">Geschiedenis</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        {/* Welcome, <a href="##" onClick={ auth.signInWithEmailAndPassword("test@test.nl", "test123") }>Mark Otto</a> */}
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
