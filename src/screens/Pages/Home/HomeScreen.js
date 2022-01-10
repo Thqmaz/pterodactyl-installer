2434d690237ac7b6e0d2e3f831174bce55de2152
Thqmaz <t.groenendijk.tg@gmail.com>
2022-01-11 00:33:27 +0100

    added the header to the homescreen for testing purposes

--- src/screens/Pages/Home/HomeScreen.js
+++ src/screens/Pages/Home/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import './HomeScreen.css'
+import { Header } from '../../Components/Header'
 
 export const HomeScreen = () => {
     return (
-        <div>
+        <>
+            <Header />
             <h1>Home</h1>
-        </div>
+        </>
     )
 }
