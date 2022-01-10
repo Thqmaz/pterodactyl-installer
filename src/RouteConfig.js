cc961857199c3b878f14302cd8275c7f680f459e
Thqmaz <t.groenendijk.tg@gmail.com>
2022-01-10 23:52:07 +0100

    updated routing with authentication checking

--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -1,35 +1,44 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { SignOut } from './functions/SignOut'
 
 // Pages
 import { HomeScreen } from './screens/Pages/Home/HomeScreen'
-
 import { InstallScreen } from './screens/Pages/Install/InstallScreen'
 import { HistoryScreen } from './screens/Pages/History/HistoryScreen'
 
 // Error pages
-import { PageNotFound } from './screens/Errors/404/PageNotFound'
-// import { ServerError } from './screens/Errors/500/ServerError'
+import { LoginScreen } from './screens/Pages/Auth/LoginScreen'
 
-export const RouteConfig = [
-    {
-        path: "/",
-        children: [
-            {
-                index: true,
-                element: <HomeScreen />
-            },
-            {
-                path: "/install",
-                element: <InstallScreen />
-            },
-            {
-                path: "/history",
-                element: <HistoryScreen />
-            },
-            {
-                path: "*",
-                element: <PageNotFound />
-            }
-        ]
-    }
-]
+
+export const RouteConfig = ({isAuth}) => [
+	{
+		path: '/',
+		children: [
+			{
+				index: true,
+				element: isAuth ? <HomeScreen /> : <Navigate to='/login' />
+			},
+			{
+				path: '/install',
+				element: isAuth ? <InstallScreen /> : <Navigate to='/login' />
+			},
+			{
+				path: '/history',
+				element: isAuth ? <HistoryScreen /> : <Navigate to='/login' />
+			},
+			{
+				path: '/login',
+				element: isAuth ? <Navigate to='/' /> : <LoginScreen />
+			},
+			{
+				path: '/signout',
+				element: isAuth ? <SignOut /> : <Navigate to='/login' />
+			}
+		],
+	},
+	{
+		path: '*',
+		element: isAuth ? <HomeScreen /> : <Navigate to='/login' />
+	}
+];
