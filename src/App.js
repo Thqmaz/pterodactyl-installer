6c954cba2a6595ee9e4344de6a9ea06d0ce0bdf0
Thqmaz <t.groenendijk.tg@gmail.com>
2022-01-10 23:51:37 +0100

    added user authentication checking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import {
-	useRoutes
-} from "react-router-dom";
+import { useState } from 'react';
+import { useRoutes } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 import { RouteConfig } from "./RouteConfig";
 
 export const App = () => {
-  let routing = useRoutes(RouteConfig);
 
+  const [user, setUser] = useState({});
+  let isAuth = false;
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  if (!user) {
+    isAuth = false;
+  } else if (user) {
+    isAuth = true;
+  };
+
+  const routing = useRoutes(RouteConfig({ isAuth }));
   return routing;
 }
