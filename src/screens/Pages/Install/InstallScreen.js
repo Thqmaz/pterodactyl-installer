1550697efed8fa52327d344c8253d7101d3420d0
Thqmaz <t.groenendijk.tg@gmail.com>
2022-01-24 17:11:38 +0100

    added the alert with credentials, dont with the panel itself!

--- src/screens/Pages/Install/InstallScreen.js
+++ src/screens/Pages/Install/InstallScreen.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
-import { Button, Col, Container, Form, Image, Row } from 'react-bootstrap'
+import { Alert, Button, Col, Container, Form, Image, Row } from 'react-bootstrap'
 import { Header } from '../../Components/Header'
 import { auth } from "../../../firebase"
 
 export const InstallScreen = () => {
     const [ipaddress, setIPAddress] = useState("")
     const [password, setPassword] = useState("")
+    const [show, setShow] = useState(false);
+    const [message, setMessage] = useState("")
+    const [code, setCode] = useState("")
+
+    // Pterodactyl credentials
+    const [loginUrl, setLoginUrl] = useState("")
+    const [loginUsername, setLoginUsername] = useState("")
+    const [loginPassword, setLoginPassword] = useState("")
+    const [databaseUser, setDatabaseUser] = useState("")
+    const [databasePassword, setDatabasePassword] = useState("")
+    const [databaseHostIp, setDatabaseHostIp] = useState(ipaddress)
+    const [databaseHostUser, setDatabaseHostUser] = useState("")
+    const [databaseHostPassword, setDatabaseHostPassword] = useState("")
+    const [databaseHostPort, setDatabaseHostPort] = useState("3306")
 
     function validateForm() {
         return ipaddress.length > 0 && password.length > 0
@@ -13,12 +27,57 @@ export const InstallScreen = () => {
 
     function handleInstallation(event) {
         event.preventDefault()
-
-        fetch(`http://localhost:4000/install/${auth.currentUser?.uid}/${ipaddress}/${ipaddress}`, { method: 'GET' })
+        fetch(`http://localhost:4000/install/${auth.currentUser?.uid}/${password}/${ipaddress}`, { method: 'GET' })
             .then(response => response.json())
             .then(response => {
-                console.log(response)
-            });
+                const obj = JSON.parse(response)
+                setMessage(obj.message)
+                setCode(obj.code)
+                if (obj.code === 200) {
+                    setLoginUrl('http://' + obj.credentials.loginUrl + "/")
+                    setLoginUsername(obj.credentials.loginUsername)
+                    setLoginPassword(obj.credentials.loginPassword)
+                    setDatabaseUser(obj.credentials.databaseUser)
+                    setDatabasePassword(obj.credentials.databasePassword)
+                    setDatabaseHostIp(obj.credentials.databaseHostIp)
+                    setDatabaseHostUser(obj.credentials.databaseHostUser)
+                    setDatabaseHostPassword(obj.credentials.databaseHostPassword)
+                    setDatabaseHostPort(obj.credentials.databaseHostPort)
+                }
+                setShow(true)
+            })
+    }
+
+    function DismissableAlert() {
+        if (show && code === 200) {
+            return (
+                <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                    <Alert.Heading>{message}</Alert.Heading>
+                    <p>
+                        URL: <a href={loginUrl}><b>{loginUrl}</b></a><br />
+                        Username: <b>{loginUsername}</b><br />
+                        Password: <b>{loginPassword}</b><br />
+                        Database User: <b>{databaseUser}</b><br />
+                        Database Password: <b>{databasePassword}</b><br />
+                        Database Host IP: <b>{databaseHostIp}</b><br />
+                        Database Host User: <b>{databaseHostUser}</b><br />
+                        Database Host Password: <b>{databaseHostPassword}</b><br />
+                        Database Host Port: <b>{databaseHostPort}</b>
+                    </p>
+                </Alert>
+            );
+        }
+        if (show && code !== 200) {
+            return (
+                <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                    <Alert.Heading>{message}</Alert.Heading>
+                    <p>
+                        kroekt
+                    </p>
+                </Alert>
+            );
+        }
+        return <></>;
     }
 
     return (
@@ -30,6 +89,7 @@ export const InstallScreen = () => {
                         <Image src="https://pterodactyl.io/logos/pterry.svg" className="img-fluid" alt="Pterodactyl Logo" />
                     </Col>
                     <Col md='8' lg='6' xl='4'>
+                        <DismissableAlert />
                         <Form onSubmit={handleInstallation}>
                             <Form.Group className="divider d-flex align-items-center my-4">
                                 <Form.Label className="text-center fw-bold mx-3 mb-0">
